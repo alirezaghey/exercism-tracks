78f3875a6cb548ddb28d49f4a55657cf797be9c6
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2019-08-06 22:00:14 +0200

    javascript RNA Transcription

---
+++ javascript/rna-transcription/rna-transcription.js
@@ -0,0 +1,17 @@
+//
+// This is only a SKELETON file for the 'RNA Transcription' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const toRna = dna =>
+  dna
+    .split('')
+    .map(c => DNA2RNA[c])
+    .join('');
+
+const DNA2RNA = {
+  G: 'C',
+  C: 'G',
+  T: 'A',
+  A: 'U'
+};
