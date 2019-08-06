5d9d792c6685db9efe2cbed5d6fc051b6546edbf
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2019-08-06 20:26:30 +0200

    removed magic number

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -3,5 +3,8 @@
 // convenience to get you started writing code faster.
 //
 
+// One giga second is equivalent to one billion seconds
+const ONEGIGASECOND = Math.pow(10, 9);
+
 export const gigasecond = oldYear =>
-  new Date(oldYear.getTime() + 1000000000000);
+  new Date(oldYear.getTime() + 1000 * ONEGIGASECOND);
