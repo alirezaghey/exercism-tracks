aeded8b525cfa9b2a9c565b8cc78ec1d05ced397
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2019-08-07 20:52:19 +0200

    javascript Pangram

---
+++ javascript/pangram/pangram.js
@@ -0,0 +1,7 @@
+//
+// This is only a SKELETON file for the 'Pangram' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const isPangram = str =>
+  new Set([...str.toLowerCase().replace(/[^a-z]/g, '')]).size === 26;
