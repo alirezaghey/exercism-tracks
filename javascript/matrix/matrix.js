6e47b34b765d3e665adfb67d8da76d774e1478b2
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2019-08-13 17:20:16 +0200

    added recursion and map verions

--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -23,12 +23,28 @@ export class Matrix {
       .split('\n')
       .map(el => el.split(' ').map(el => Number(el)));
     this._rows = arrMatrix.slice();
-    for (let i = 0; i < arrMatrix[0].length; i++) {
-      const tempCol = [];
-      for (let j = 0; j < arrMatrix.length; j++) {
-        tempCol.push(arrMatrix[j][i]);
-      }
-      this._cols.push(tempCol);
-    }
+
+    // Using recursion to transpose
+    // this._cols = this.transpose(arrMatrix);
+
+    // Using nested maps to transpose
+    this._cols = arrMatrix[0].map((_, i) =>
+      arrMatrix.map((_, j) => arrMatrix[j][i])
+    );
+
+    // Using plain old for loop to transpose
+    // for (let i = 0; i < arrMatrix[0].length; i++) {
+    //   const tempCol = [];
+    //   for (let j = 0; j < arrMatrix.length; j++) {
+    //     tempCol.push(arrMatrix[j][i]);
+    //   }
+    //   this._cols.push(tempCol);
+    // }
+  }
+  transpose(matrix) {
+    const row = x => (x >= matrix.length ? [] : [col(x, 0), ...row(x + 1)]);
+    const col = (x, y) =>
+      y >= matrix.length ? [] : [matrix[y][x], ...col(x, y + 1)];
+    return row(0);
   }
 }
