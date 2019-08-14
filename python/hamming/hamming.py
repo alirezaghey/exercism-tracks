95f477c6d1c1a32398357c56686a4d7245ddf7a4
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2019-08-14 10:18:23 +0200

    python Hamming ex.

---
+++ python/hamming/hamming.py
@@ -0,0 +1,23 @@
+from functools import reduce
+
+
+def distance(strand_a: str, strand_b: str) -> int:
+    if len(strand_a) != len(strand_b):
+        raise ValueError(
+            "Comparing strands need to have the same length for a valid Hamming Distance")
+
+    # Using sum, beautiful list comprehension with where clause and dangerous zip
+    return sum([1 for x, y in zip(strand_a, strand_b) if x != y])
+
+    # Using sum, beautiful list comprehension and dangerous zip
+    # return sum([1 if x != y else 0 for x, y in zip(strand_a, strand_b)])
+
+    # using badass reduce, lambda, beautiful list comprehension and dangerous zip
+    # return reduce(lambda x, y: x+y, [1 for x, y in zip(strand_a, strand_b) if x != y], 0)
+
+    # Using good old for loop and dangerous zip
+    # result: int = 0
+    # for x, y in zip(strand_a, strand_b):
+    #     if x != y:
+    #         result += 1
+    # return result
