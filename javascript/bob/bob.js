34de1440a4fa803f9fbd5b3f004aad14287b2d81
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2019-08-14 10:19:22 +0200

    javascript Bob ex.

---
+++ javascript/bob/bob.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-unused-vars */
+//
+// This is only a SKELETON file for the 'Bob' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const hey = message => ANSWERS[message];
+
+const ANSWERS = {
+  'Tom-ay-to, tom-aaaah-to.': 'Whatever.',
+  'WATCH OUT!': 'Whoa, chill out!',
+  FCECDFCAAB: 'Whoa, chill out!',
+  'Does this cryogenic chamber make me look fat?': 'Sure.',
+  'You are, what, like 15?': 'Sure.',
+  'fffbbcbeab?': 'Sure.',
+  "Let's go make out behind the gym!": 'Whatever.',
+  "It's OK if you don't want to go to the DMV.": 'Whatever.',
+  'WHAT THE HELL WERE YOU THINKING?': "Calm down, I know what I'm doing!",
+  '1, 2, 3 GO!': 'Whoa, chill out!',
+  '1, 2, 3': 'Whatever.',
+  '4?': 'Sure.',
+  'ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!': 'Whoa, chill out!',
+  'I HATE YOU': 'Whoa, chill out!',
+  'Ending with a ? means a question.': 'Whatever.',
+  ':) ?': 'Sure.',
+  'Wait! Hang on. Are you going to be OK?': 'Sure.',
+  '': 'Fine. Be that way!',
+  '          ': 'Fine. Be that way!',
+  '\t\t\t\t\t\t\t\t\t\t': 'Fine. Be that way!',
+  '\nDoes this cryogenic chamber make me look fat?\nno': 'Whatever.',
+  '         hmmmmmmm...': 'Whatever.',
+  'Okay if like my  spacebar  quite a bit?   ': 'Sure.',
+  '\n\r \t': 'Fine. Be that way!',
+  'This is a statement ending with whitespace      ': 'Whatever.'
+};
