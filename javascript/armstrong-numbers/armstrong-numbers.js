9ca1095004fd69817ec5f954a094cf502f813f2a
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2019-08-14 10:26:30 +0200

    update Armstrong

--- javascript/armstrong-numbers/armstrong-numbers.js
+++ javascript/armstrong-numbers/armstrong-numbers.js
@@ -4,13 +4,20 @@
 //
 
 export const validate = num => {
-  const numOfDigits = (num + '').length;
-  let numTemp = num;
-  let sum = 0;
-  while (numTemp > 0) {
-    sum += (numTemp % 10) ** numOfDigits;
-    numTemp = Math.floor(numTemp / 10);
-  }
+  const numOfDigits = String(num).length;
+  return (
+    num ===
+    String(num)
+      .split('')
+      .reduce((acc, el) => acc + Math.pow(Number(el), numOfDigits), 0)
+  );
+  // const numOfDigits = (num + '').length;
+  // let numTemp = num;
+  // let sum = 0;
+  // while (numTemp > 0) {
+  //   sum += (numTemp % 10) ** numOfDigits;
+  //   numTemp = Math.floor(numTemp / 10);
+  // }
 
-  return sum === num;
+  // return sum === num;
 };
