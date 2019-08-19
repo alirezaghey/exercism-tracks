0afd8f1538933c782803b3e04895732ebd998c2d
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2019-08-19 17:20:57 +0200

    update bob.js

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -5,28 +5,30 @@
 //
 
 export const hey = message => {
+  const ANSWERS = {
+    whatever: { regex: [], response: 'Whatever.' },
+    whoa: {
+      regex: [
+        /(^[A-Z ]+!$)/,
+        /(^[A-Z]+$)/,
+        /(^[\d,A-Z ]+!$)/,
+        /(^[A-Z\d\W]+!$)/,
+        /(^([A-Z]+[ ]*)+$)/
+      ],
+      response: 'Whoa, chill out!'
+    },
+    calm: {
+      regex: [/^[A-Z ]+\?$/],
+      response: "Calm down, I know what I'm doing!"
+    },
+    sure: {
+      regex: [/([\w\d]+\?$)/, /([\W]+\?$)/, /(\? +$)/],
+      response: 'Sure.'
+    },
+    fine: { regex: [/(^\s+$)/, /(^$)/], response: 'Fine. Be that way!' }
+  };
   for (let k in ANSWERS)
     if (ANSWERS[k].regex.some(reg => reg.test(message)))
       return ANSWERS[k].response;
   return ANSWERS.whatever.response;
-};
-
-const ANSWERS = {
-  whatever: { regex: [], response: 'Whatever.' },
-  whoa: {
-    regex: [
-      /(^[A-Z ]+!$)/,
-      /(^[A-Z]+$)/,
-      /(^[\d,A-Z ]+!$)/,
-      /(^[A-Z\d\W]+!$)/,
-      /(^([A-Z]+[ ]*)+$)/
-    ],
-    response: 'Whoa, chill out!'
-  },
-  calm: {
-    regex: [/^[A-Z ]+\?$/],
-    response: "Calm down, I know what I'm doing!"
-  },
-  sure: { regex: [/([\w\d]+\?$)/, /([\W]+\?$)/, /(\? +$)/], response: 'Sure.' },
-  fine: { regex: [/(^\s+$)/, /(^$)/], response: 'Fine. Be that way!' }
 };
