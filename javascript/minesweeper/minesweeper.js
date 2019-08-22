89fd7a373eeed7b4d5e3d90cd43f23b3b0248ebb
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2019-08-22 18:32:37 +0200

    refactoring

--- javascript/minesweeper/minesweeper.js
+++ javascript/minesweeper/minesweeper.js
@@ -9,37 +9,61 @@
 //          We could potentially create new strings for the rows and change them at the end of annotating each row
 //          which would decrease our space complexity by a factor of the number of the rows but considering
 //          that it would make our code much more complext we have decided not to do so.
-const MINE = '*';
-export const annotate = rawField =>
-  rawField.map((row, i) =>
-    row
-      .split('')
-      .map((_, j) => {
-        return rawField[i][j] === MINE
-          ? MINE
-          : findAdjacentMines(rawField, i, j);
-      })
-      .join('')
-  );
-
-// from top-left corner moves around a cell clockwise
-// where adjCells[0][0] is the horizontal offset and
-// adjCells[0][1] is the vertical offset
-//prettier-ignore
-const adjCells = [[-1,-1],  [0, -1],  [1,-1],
-                  [-1, 0],  /*^|^*/   [1, 0],
-                  [-1, 1],  [0, 1],   [1, 1]]
-// Using the adjCells we check all the adjacent cells
-// and increase the numOfMines accordingly
-const findAdjacentMines = (field, y, x) => {
-  const numOfMines = adjCells.reduce(
-    (acc, [xOffset, yOffset]) =>
-      field[y + yOffset] && field[y + yOffset][x + xOffset] === MINE
-        ? ++acc
-        : acc,
-    0
-  );
-  // if number of adjacent mines equals zero
-  // we need to enter an empty space for that particular cell
-  return numOfMines === 0 ? ' ' : numOfMines + '';
+export const annotate = field => {
+  if (field.length === 0) return [];
+  const width = field[0].length;
+  const heigth = field.length;
+  const mineField = new MineField(heigth, width);
+  for (let y = 0; y < heigth; y++) {
+    for (let x = 0; x < width; x++) {
+      mineField.setCell(field, y, x);
+    }
+  }
+  return mineField.grid.map(row => row.join(''));
 };
+
+class MineField {
+  constructor(heigth, width) {
+    this.grid = this.initGrid(heigth, width);
+    // from top-left corner moves around a cell clockwise
+    // where adjCells[0][0] is the horizontal offset and
+    // adjCells[0][1] is the vertical offset
+    //prettier-ignore
+    this.adjCells = [[-1,-1],  [0, -1],  [1,-1],
+                     [-1, 0],  /*^|^*/   [1, 0],
+                     [-1, 1],  [0, 1],   [1, 1]]
+    this.MINE = '*';
+    this.BLANK = ' ';
+  }
+
+  setCell(field, y, x) {
+    if (field[y][x] === this.MINE) {
+      this.grid[y][x] = this.MINE;
+      return;
+    }
+
+    const numOfMines = this.findAdjacenBombs(field, y, x);
+
+    // if numOfMines is zero, insert blank
+    this.grid[y][x] = numOfMines ? numOfMines.toString() : this.BLANK;
+  }
+
+  // Return all the surrounding cells' content
+  findAdjacenBombs(field, y, x) {
+    return this.adjCells
+      .map(([xOffset, yOffset]) =>
+        field[y + yOffset] && field[y + yOffset][x + xOffset]
+          ? field[y + yOffset][x + xOffset]
+          : ''
+      )
+      .filter(el => el === this.MINE).length;
+  }
+
+  initGrid(height, width) {
+    const matrix = [];
+    for (let y = 0; y < height; y++) {
+      matrix.push(new Array(width));
+    }
+    return matrix;
+  }
+}
