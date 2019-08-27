1cef91db6e336fc91090fb41e7a47fc6749fcd62
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2019-08-27 06:34:42 +0200

    refactoring

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -4,4 +4,4 @@ from typing import List, Dict
 def convert(number: int) -> str:
     lookup: Dict[int, str] = {3: "Pling", 5: "Plang", 7: "Plong"}
     result: List[str] = [lookup[key] for key in lookup if number % key == 0]
-    return "".join(result) if len(result) else str(number)
+    return "".join(result) if result else str(number)
