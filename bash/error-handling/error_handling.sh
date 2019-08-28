5c2cdf0de747178aafbd3a430a7a20ba4836b6e7
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2019-08-28 07:41:30 +0200

    added Error Handling

---
+++ bash/error-handling/error_handling.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+main () {
+    if [[ "$#" -ne 1 ]]; then
+        echo "Usage: ./error_handling <greetee>"
+        return 1
+    else
+        echo "Hello, $1"
+    fi
+}
+
+main "$@"
