62912adc01f59a40214e3c606bead897cf556e10
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2019-08-30 11:27:04 +0200

    added comments; made code more readable

--- javascript/pascals-triangle/pascals-triangle.js
+++ javascript/pascals-triangle/pascals-triangle.js
@@ -1,31 +1,40 @@
-//
-// This is only a SKELETON file for the 'Pascals Triangle' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class Triangle {
+  /**
+   *
+   * @param {number} n Number of rows in Pascal's triangle
+   */
   constructor(n) {
-    this._rows = this.init(n);
+    this._rows = this._init(n);
   }
 
+  /**
+   * @returns {[number]}  Last row in Pascal's triangle
+   */
   get lastRow() {
     return [...this._rows[this._rows.length - 1]];
   }
 
+  /**
+   * @returns {[[number]]}  All the rows of Pascal's triangle
+   */
   get rows() {
     return [...this._rows];
   }
-
-  init(n) {
-    const r = [[1]];
+  /**
+   *
+   * @param {number} n Number of rows in Pascal's triangle
+   * @returns {[[Number]]} All the rows of Pascal's triangle built based on n
+   */
+  _init(n) {
+    const result = [[1]];
     for (let i = 0; i < n - 1; i++) {
-      const temp = [1];
+      const tempRow = [1];
       for (let j = 1; j < i + 1; j++) {
-        temp.push(r[i][j - 1] + r[i][j]);
+        tempRow.push(result[i][j - 1] + result[i][j]);
       }
-      temp.push(1);
-      r.push(temp);
+      tempRow.push(1);
+      result.push(tempRow);
     }
-    return r;
+    return result;
   }
 }
