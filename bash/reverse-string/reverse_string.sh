c5e46c24d86d0d5e835e2cdccd10e836bfc72792
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2019-09-03 22:56:06 +0200

    refactored reverse_string.sh

--- bash/reverse-string/reverse_string.sh
+++ bash/reverse-string/reverse_string.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
 main() {
-    
-    len=${#1}
-    for ((i=$len-1; i>=0; i--))
-        do rev="$rev${1:$i:1}"
+    local len=${#1}
+    local rev
+    for ((i=$len-1; i>=0; i--)); do
+        rev+=${1:$i:1}
     done
-    echo $rev
-    status=0
+    echo "$rev"
     
     # cheating using an external tool
     # echo $1 | rev
