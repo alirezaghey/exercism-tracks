1c217833126f03bae4c30aeeab0f9eaef819793e
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2019-09-03 22:43:58 +0200

    refactored leap.sh

--- bash/leap/leap.sh
+++ bash/leap/leap.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 
 
-  main () {
+main () {
 
-    # Num of arguments == 1
-    if [[ "$#" -ne 1 ]] ||
-    # Not a float
-    [[ $1 =~ ^[0-9]+\.[0-9]+$ ]] ||
-    # No alpha chars
-    [[ $1 =~ [a-zA-Z] ]]; then
-        echo "Usage: leap.sh <year>"
-        return 1
-    elif !(($1 % 400)) || (!(($1 % 4)) && (($1 % 100))); then
-        echo "true"
-    else
-        echo "false"
-    fi
-  }
+  # Num of arguments == 1
+  # Not a float
+  # No alpha chars
+  if  [[ "$#" -ne 1 ]] ||
+      [[ $1 =~ ^[0-9]+\.[0-9]+$ ]] ||
+      [[ $1 =~ [a-zA-Z] ]];
+  then
+      echo "Usage: leap.sh <year>"
+      return 1
+  elif !(($1 % 400)) || (!(($1 % 4)) && (($1 % 100))); then
+      echo "true"
+  else
+      echo "false"
+  fi
+}
 
-    main "$@"
+  main "$@"
