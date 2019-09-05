340526374b0d0109922e25864f62d0070789d336
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2019-09-05 18:10:47 +0200

    refactored bash Resistor Color Duo

--- bash/resistor-color-duo/resistor_color_duo.sh
+++ bash/resistor-color-duo/resistor_color_duo.sh
@@ -2,18 +2,10 @@
 
 
 main () {
-    declare -A colors
-    colors[black]="0"
-    colors[brown]="1"
-    colors[red]="2"
-    colors[orange]="3"
-    colors[yellow]="4"
-    colors[green]="5"
-    colors[blue]="6"
-    colors[violet]="7"
-    colors[grey]="8"
-    colors[white]="9"
-
+    local -A colors=(
+        [black]=0	    [brown]=1	    [red]=2	    [orange]=3	    [yellow]=4
+        [green]=5	    [blue]=6	    [violet]=7	[grey]=8	    [white]=9	
+    )
     if [[ -v "colors[$1]" ]] && [[ -v "colors[$2]" ]] ; then
         echo ${colors[$1]}${colors[$2]}
     else
