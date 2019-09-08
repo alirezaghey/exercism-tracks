9889ff184066b4d96924dbe0cfacaac10ca8270d
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2019-09-08 22:02:38 +0200

    refactored bash Nucleotide Count

--- bash/nucleotide-count/nucleotide_count.sh
+++ bash/nucleotide-count/nucleotide_count.sh
@@ -1,27 +1,22 @@
 #!/usr/bin/env bash
 
 main () {
-    local A=0
-    local C=0
-    local G=0
-    local T=0
-
+    local -A types=(
+        [A]=0   [C]=0   [G]=0    [T]=0
+    )
+    # loop over each and ever char of input string
     for (( i=0; i<${#1}; i++ )); do
-        if [[ ${1:$i:1} == "A" ]]; then
-            A=$(( A+1 ))
-        elif [[ ${1:$i:1} == "C" ]]; then
-            C=$(( C+1 ))
-        elif [[ ${1:$i:1} == "G" ]]; then
-            G=$(( G+1 ))
-        elif [[ ${1:$i:1} == "T" ]]; then
-            T=$(( T+1 ))
+        # check if it's among the keys of types
+        if [[ " ${!types[*]} " =~ " ${1:$i:1} " ]]; then
+            # increment corresponding value
+            ((types["${1:$i:1}"]++))
         else
             echo "Invalid nucleotide in strand"
             exit 1
         fi
     done
 
-    echo -e $"A: $A\nC: $C\nG: $G\nT: $T"
+    echo -e $"A: ${types[A]}\nC: ${types[C]}\nG: ${types[G]}\nT: ${types[T]}"
 }
 
 main "$@"
