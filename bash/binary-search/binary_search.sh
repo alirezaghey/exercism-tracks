18bdf214966a5abc67b1671c42fd812cae591cc4
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2019-09-12 22:03:45 +0200

    added bash Binary Search

---
+++ bash/binary-search/binary_search.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+
+arg1=$1; shift
+array=( "$@" )
+left=0
+right="${#array[@]}"
+
+while (( $left <= $right )); do
+    (( mid=(($right-$left) / 2) + $left ))
+    if [[ "${array[$mid]}" -eq "$arg1" ]]; then
+        echo $mid
+        exit 0
+    elif [[ "${array[$mid]}" -lt "$arg1" ]]; then
+        (( left="$mid+1" ))
+    else
+        (( right="$mid-1" ))
+    fi
+done
+    echo -1
