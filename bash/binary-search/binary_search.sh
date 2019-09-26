3a19ca494da3aefdfe3f158a9d384bf3fa73a666
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2019-09-26 19:59:39 +0200

    updated bash Binary Search

--- bash/binary-search/binary_search.sh
+++ bash/binary-search/binary_search.sh
@@ -7,14 +7,14 @@ left=0
 right="${#array[@]}"
 
 while (( $left <= $right )); do
-    (( mid=(($right-$left) / 2) + $left ))
+    (( mid= (right - left) / 2 + left ))
     if [[ "${array[$mid]}" -eq "$arg1" ]]; then
         echo $mid
         exit 0
     elif [[ "${array[$mid]}" -lt "$arg1" ]]; then
-        (( left="$mid+1" ))
+        (( left = mid + 1 ))
     else
-        (( right="$mid-1" ))
+        (( right = mid - 1 ))
     fi
 done
     echo -1
