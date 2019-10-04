a0487cbf3e30c3938b3d96340cd6b88162cf30b8
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2019-10-04 15:56:28 +0200

    added bash Scrabble Score

---
+++ bash/scrabble-score/scrabble_score.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+main () {
+    local -A points=(   [A]=1 [E]=1 [I]=1 [O]=1 [U]=1 [L]=1 [N]=1 [R]=1 [S]=1 [T]=1
+                        [D]=2 [G]=2
+                        [B]=3 [C]=3 [M]=3 [P]=3
+                        [F]=4 [H]=4 [V]=4 [W]=4 [Y]=4
+                        [K]=5
+                        [J]=8 [X]=8
+                        [Q]=10 [Z]=10)
+
+    local chars=${1^^}
+    local result=0
+    for (( i=0; i<${#1}; i++ )); do
+        result=$(( result+${points[${chars:i:1}]} ))
+    done
+    echo $result
+}
+
+main "$@"
