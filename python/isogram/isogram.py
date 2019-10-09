adcd5d88cd061d8a999e187610eb22944153d9bb
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2019-10-09 07:20:48 +0200

    refactored isogram.py

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -2,5 +2,5 @@ import re
 
 
 def is_isogram(string: str) -> bool:
-    string = re.sub(("[^a-z]"), "", string.lower())
+    string = [c.lower() for c in string if c.isalpha()]
     return len(string) == len({c for c in string})
